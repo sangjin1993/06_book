850d6f5e775d71b6bb513d116e4545ee816b97fd
sangjin1993 <50225008+sangjin1993@users.noreply.github.com>
2019-09-18 14:49:39 +0900

    vo, exception

---
+++ 02_java/book/src/book/exception/DuplicateException.java
@@ -0,0 +1,20 @@
+package book.exception;
+
+import book.vo.Book;
+
+/**
+ * Book 데이터 신규 입력시 책 정보 중복될 때
+ * 발생시킬 예외 정의 클래스
+ */
+public class DuplicateException extends Exception {
+
+	// 기본 생성자
+	public DuplicateException() {
+		super("도서정보가 중복되었습니다.");
+	}
+	
+	// 중복정의 생성자
+	public DuplicateException(String type, Book book) {
+		super(String.format("%s:[bookSeq%n] 도서정보가 중복되었습니다.", type, book.getBookSeq().intValue()));
+	}
+}
---
+++ 02_java/book/src/book/vo/Book.java
@@ -0,0 +1,153 @@
+package book.vo;
+
+import java.math.BigInteger;
+
+public class Book {
+	
+	private BigInteger bookSeq;
+	private String isbn;
+	private String title;
+	private String author;
+	private String content;
+	private BigInteger companyCd;
+	private BigInteger totalPage;
+	private BigInteger quantity;
+	private String regId;
+	private String regDate;
+	private String modId;
+	private String modDate;
+	
+	public Book () {
+		super();
+	}
+
+	public BigInteger getBookSeq() {
+		return bookSeq;
+	}
+
+	public void setBookSeq(BigInteger bookSeq) {
+		this.bookSeq = bookSeq;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public BigInteger getCompanyCd() {
+		return companyCd;
+	}
+
+	public void setCompanyCd(BigInteger companyCd) {
+		this.companyCd = companyCd;
+	}
+
+	public BigInteger getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(BigInteger totalPage) {
+		this.totalPage = totalPage;
+	}
+
+	public BigInteger getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(BigInteger quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getRegId() {
+		return regId;
+	}
+
+	public void setRegId(String regId) {
+		this.regId = regId;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public String getModId() {
+		return modId;
+	}
+
+	public void setModId(String modId) {
+		this.modId = modId;
+	}
+
+	public String getModDate() {
+		return modDate;
+	}
+
+	public void setModDate(String modDate) {
+		this.modDate = modDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((bookSeq == null) ? 0 : bookSeq.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Book other = (Book) obj;
+		if (bookSeq == null) {
+			if (other.bookSeq != null)
+				return false;
+		} else if (!bookSeq.equals(other.bookSeq))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "책 정보 [bookSeq=" + bookSeq + ", isbn=" + isbn + ", title=" + title + ", author=" + author + ", content="
+				+ content + ", companyCd=" + companyCd + ", totalPage=" + totalPage + ", quantity=" + quantity
+				+ ", regId=" + regId + ", regDate=" + regDate + ", modId=" + modId + ", modDate=" + modDate + "]";
+	}
+	
+	
+}
